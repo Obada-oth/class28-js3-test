398d83b8161b6b01d2a2f83511340514ebd2ea6b
obada othman <obada.oth@gmail.com>
2020-10-04 18:17:27 +0200

    Added new fetch api to retrieve more data about each movie

--- script.js
+++ script.js
@@ -15,21 +15,36 @@ function app() {
   const movieListBtn = document.createElement("select");
   movieListBtn.id = "select";
   listDiv.appendChild(movieListBtn);
+
+  const choosePropmt = document.createElement("option");
+  choosePropmt.innerText = "---Choose a movie---";
+  choosePropmt.setAttribute("selected", "selected");
+  movieListBtn.appendChild(choosePropmt);
+
   function getMovies() {
     fetch("http://www.omdbapi.com/?apikey=62e525bd&s=batman")
       .then((res) => res.json())
       .then((batmanMovies) => {
+        console.log(batmanMovies);
         let movieListArr = batmanMovies.Search;
         console.log(movieListArr);
 
         movieListArr.forEach((movie) => {
           const movieTitle = document.createElement("option");
           movieTitle.innerText = movie.Title;
-          movieTitle.value = `
-            <img src="${movie.Poster}">
-            <h3 class="details">${movie.Title}</h3>
-            <h4 class="details">Year : ${movie.Year}</h4>
+          fetch(`http://www.omdbapi.com/?apikey=62e525bd&i=${movie.imdbID}`)
+            .then((res) => res.json())
+            .then((batmanMovie) => {
+              console.log(batmanMovie);
+              movieTitle.value = `
+            <img class = "poster" src="${batmanMovie.Poster}">
+            <h3 class="details">${batmanMovie.Title}</h3>
+            <h3 class="details">Directed By : ${batmanMovie.Director}</h3>
+            <h4 class="details">Year : ${batmanMovie.Year}</h4>
+            <p class="details"> Plot : ${batmanMovie.Plot}</p>
             `;
+            });
+
           movieListBtn.appendChild(movieTitle);
         });
       })
